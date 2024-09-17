20439b5faa58c9c36625001033249feaf10eac62
ALex Nikonov <alex@nikonov.tech>
2024-09-18 01:15:22 +0400

    feat: create tasks from os.Args, list if none

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"sort"
+	"strconv"
+	"strings"
 
 	"gitlab.com/nikonov1101/go-for-milk/rtm"
 )
@@ -16,26 +19,39 @@ func main() {
 		panic(err)
 	}
 
-	// add a task to the Inbox, note that smart parsing works here, so #^! are turned into the task meta-data
-	if err := cli.AddTask("this is my new task using my new client #random !2 ^tomorrow"); err != nil {
-		panic(err)
+	// invoked with some args, lets make a task from them,
+	// keep in mind that priority is set with "!n", where n is 1..3,
+	// but zsh requires the escape of "!", so it will looks like this:
+	// go run main.go this is important stuff \!2
+	if len(os.Args) > 1 {
+		name := strings.Join(os.Args[1:], " ")
+		log.Printf("name = %q", name)
+		if err := cli.AddTask(name); err != nil {
+			panic(err)
+		}
+
+		log.Printf("task added.")
+		os.Exit(0)
 	}
 
-	// query back your tasks, returns all items from all list
-	// as a single, flat array, for details see implementation in rtm/types.go @ intoTasks()
+	// no args given, just list all tasks
 	tasks, err := cli.ListTasks()
 	if err != nil {
 		panic(err)
 	}
 
-	// client does not sort tasks, but we can do so by any criteria we want
 	sort.Slice(tasks, func(i, j int) bool {
-		return tasks[i].Priority > tasks[j].Priority
+		if tasks[i].Priority != tasks[j].Priority {
+			// sort by priority first
+			return tasks[i].Priority > tasks[j].Priority
+		}
+		// then by created_at, if priorities are equal
+		return tasks[i].CreatedAt.After(tasks[j].CreatedAt)
 	})
 
 	for _, task := range tasks {
 		if task.Visible() { // task is not visible if it's completed OR deleted
-			log.Printf("[%d] %s :: #%v", task.Priority, task.Name, task.Tags)
+			fmt.Println(colorizeTask(task))
 		}
 	}
 }
@@ -54,4 +70,61 @@ func loadKeys() (string, string) {
 	}
 
 	return key, secret
+}
+
+func colorizeTask(t rtm.Task) string {
+	prioNum := ""
+	text := t.Name
+	switch t.Priority {
+	case 0:
+		prioNum = white(" ")
+	case 1:
+		prioNum = green("!")
+	case 2:
+		prioNum = yellow("!")
+	case 3:
+		prioNum = red("!")
+		text = white(text)
+	default:
+		return strconv.Itoa(t.Priority)
+	}
+
+	tags := ""
+	if len(t.Tags) > 0 {
+		tmp := make([]string, len(t.Tags))
+		for i := range t.Tags {
+			tmp[i] = "#" + t.Tags[i]
+		}
+		tags = " " + gray(strings.Join(tmp, ", "))
+	}
+	return fmt.Sprintf("[%s] %s%s", prioNum, text, tags)
+}
+
+const (
+	RED = "\033[1;31m"
+	YEL = "\033[1;33m"
+	GRE = "\033[1;32m"
+	WHT = "\033[1;37m"
+	GRY = "\033[1;30m"
+	NC  = "\033[0m" // No Color
+)
+
+func green(s string) string {
+	return GRE + s + NC
+}
+
+func yellow(s string) string {
+	return YEL + s + NC
+}
+
+func red(s string) string {
+	return RED + s + NC
+}
+
+func white(s string) string {
+	return WHT + s + NC
+}
+
+func gray(s string) string {
+	return GRY + s + NC
 }
